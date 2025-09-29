42fe21a2e45866159a4a04a0e68be62a26bbeaea
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-09-29 11:06:07 -0300

    modelos de sequilize terminado

---
+++ src/models/sequelize/asset_category.model.js
@@ -0,0 +1,25 @@
+import { DataTypes } from "sequelize";
+import { AssetModel } from "./asset.model.js";
+import { CategoryModel } from "./category.model.js";
+
+export const AssetCategoryModel = sequelize.define("AssetCategory", {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+});
+
+// TODO: completar relaciones muchos a muchos entre Asset y Category mediante AssetCategory.
+// * N:M Asset ↔ Category through AssetCategory
+// * 'categories' (Asset) y 'assets' (Category)
+// ! FALTA COMPLETAR ACA
+AssetModel.belongsToMany(CategoryModel, {
+  through: AssetCategory,
+  foreignKey: "asset_id",
+  as: "category",
+  onDelete: "CASCADE",
+});
+
+CategoryModel.belongsToMany(AssetModel, {
+  through: AssetCategory,
+  foreignKey: "category_id",
+  as: "asset",
+  onDelete: "CASCADE",
+});
