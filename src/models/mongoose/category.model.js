588c318a0f4180337007c7adef7c1cba7dca4e16
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-09-29 11:04:49 -0300

    modelo de categoria terminado

---
+++ src/models/mongoose/category.model.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+
+// TODO: configurar el virtuals para el populate inverso con assets
+
+const CategorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+    description: { type: String, maxlength: 500 },
+  },
+  { timestamps: true }
+);
+
+// ! FALTA COMPLETAR ACA
+CategorySchema.virtual("assets", {
+  ref: "Asset",
+  localField: "_id",
+  foreignField: "categories",
+});
+// para que aparezcan en formato json
+CategorySchema.set("toObject", { virtuals: true });
+CategorySchema.set("toJSON", { virtuals: true });
+
+//eliminacion en cascada
+CategorySchema.pre("findOneAndDelete", async function (next) {
+  const category = await this.model.findOne(this.getFilter());
+  if (category) {
+    await AssetModel.updateMany(
+      { categories: category._id },
+      { $pull: { categories: category._id } }
+    );
+  }
+  next();
+});
+export const CategoryModel = model("Category", CategorySchema);
